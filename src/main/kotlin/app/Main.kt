d7e2de94d69a29e33ba7a50096ffce1c600d9284
Marten <persson_dunk@msn.com>
2017-10-05 22:41:33 +0200

    Able to create new Song object, new method for cross origin,javalin error removed now logging.

--- src/main/kotlin/app/Main.kt
+++ src/main/kotlin/app/Main.kt
@@ -2,46 +2,59 @@ package app
 
 import io.javalin.Javalin
 import java.util.concurrent.atomic.AtomicInteger
+import com.fasterxml.jackson.module.kotlin.*
+import io.javalin.Context
+
 
 data class Song(val  songName: String, val artist: String,
                 var points: Int, var currentPoints: Int, var startNumber: Int)
+
+val mapper = jacksonObjectMapper()
+
 
 fun main(args: Array<String>)  {
 
     val userDao = UserDao()
     val app = Javalin.create().port(7000).start()
-    val filter = CorsFilter()
 
     with(app) {
 
         get("/") { ctx ->
-            ctx.response().setHeader("Access-Control-Allow-Origin", "*")
+            allowCrossOrigin(ctx)
             ctx.json("Hello Everybody")
         }
 
         get("/songs") { ctx ->
-            ctx.response().setHeader("Access-Control-Allow-Origin", "*")
+            allowCrossOrigin(ctx)
             ctx.json(userDao.songs)
             println("Försöker iaf")
         }
 
         get("/songs/:startNumber") { ctx ->
+            allowCrossOrigin(ctx)
             ctx.json(userDao.findByStartNumber(ctx.param("startNumber")!!.toInt())!!)
         }
 
         get("/songs/artist/:artist") { ctx ->
+            allowCrossOrigin(ctx)
             ctx.json(userDao.findByArtist(ctx.param("artist")!!)!!)
         }
 
         post("/songs/create") { ctx ->
-            val song = ctx.bodyAsClass(Song::class.java)
+            allowCrossOrigin(ctx)
+            val song = mapper.readValue<Song>(ctx.body())
+            println(song)
+
             userDao.save(songName = song.songName, artist = song.artist, points = song.points,
                     currentPoints = song.currentPoints)
             ctx.status(201)
         }
 
-        patch("/songs/update/:startNumber") { ctx ->
-            val song = ctx.bodyAsClass(Song::class.java)
+        put("/songs/update/:startNumber") { ctx ->
+            allowCrossOrigin(ctx)
+            val song = mapper.readValue<Song>(ctx.body())
+            println("Försöker uppdatera $song")
+            //val song = ctx.bodyAsClass(Song::class.java)
             userDao.update(
                     startNumber = ctx.param("startNumber")!!.toInt(),
                     song = song
@@ -64,25 +77,34 @@ fun main(args: Array<String>)  {
     }
 }
 
+/**
+ *
+ */
+fun allowCrossOrigin(ctx: Context) {
+    ctx.response().setHeader("Access-Control-Allow-Origin", "*")
+    ctx.response().setHeader("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS")
+}
+
 class UserDao {
 
     val songs = hashMapOf(
             0 to Song(songName = "Forever Young", artist = "Morre and the boys",
-                    points = 0, currentPoints = 0, startNumber = 1),
+                    points = 0, currentPoints = 0, startNumber = 0),
             1 to Song(songName = "Never old", artist = "The capitals",
-                    points = 0, currentPoints = 0, startNumber = 2),
+                    points = 0, currentPoints = 0, startNumber = 1),
             2 to Song(songName = "Always everytime", artist = "Rihanna",
-                    points = 0, currentPoints = 0, startNumber = 3),
+                    points = 0, currentPoints = 0, startNumber = 2),
             3 to Song(songName = "Me and the you", artist = "Rammstein",
-                    points = 0, currentPoints = 0, startNumber = 4)
+                    points = 0, currentPoints = 0, startNumber = 3)
     )
 
-    var lastId: AtomicInteger = AtomicInteger(songs.size - 1)
+    var lastId: AtomicInteger = AtomicInteger(songs.size -1)
 
     fun save(songName: String, artist: String, points: Int, currentPoints: Int) {
         val startNumber = lastId.incrementAndGet()
         songs.put(startNumber, Song(songName = songName, artist = artist, startNumber = startNumber,
                 currentPoints = currentPoints, points = points))
+        println(songs)
     }
 
     fun findByStartNumber(startNumber: Int): Song? {
@@ -94,8 +116,14 @@ class UserDao {
     }
 
     fun update(startNumber: Int, song: Song) {
-        songs.put(startNumber, Song(songName = song.songName, artist = song.artist,
-                points = song.points, currentPoints = song.currentPoints, startNumber = startNumber))
+        for (x in songs.keys){
+            if (x.equals(startNumber)){
+                songs[x] = Song(startNumber = startNumber, artist = song.artist, songName = song.songName,
+                        points = song.points, currentPoints = song.currentPoints)
+            }
+        }
+        //songs.put(startNumber, Song(songName = song.songName, artist = song.artist,
+               // points = song.points, currentPoints = song.currentPoints, startNumber = startNumber))
     }
 
     fun delete(startNumber: Int) {
